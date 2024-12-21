6351c76db3d1feff975dd4b146c070c39a3df3dc
pierre <pierregobin@free.Fr>
2024-12-21 07:17:21 +0100

    remove current_line_blame_opts_formatter

--- lua/git-config.lua
+++ lua/git-config.lua
@@ -34,15 +34,14 @@ gitsigns.setup {
         follow_files = true,
     },
     attach_to_untracked = true,
-    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    -- current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
         delay = 1000,
         ignore_whitespace = false,
-    },
-    current_line_blame_formatter_opts = {
-        relative_time = false,
+        virt_text_priority = 100,
+        use_focus = true,
     },
     sign_priority = 6,
     update_debounce = 100,
@@ -56,7 +55,7 @@ gitsigns.setup {
         row = 0,
         col = 1,
     },
---    yadm = {
---        enable = false,
---    },
+    --    yadm = {
+    --        enable = false,
+    --    },
 }
