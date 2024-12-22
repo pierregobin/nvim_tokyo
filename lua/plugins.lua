ea9712b80b13fd1d96bd2767987bf584d441b80d
pierre <pierregobin@free.Fr>
2024-12-22 09:12:10 +0100

    add nvim-treesitter with right options

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -89,7 +89,21 @@ return {
     },
     -- Treesitter
     {
-        "nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup {
+			highlight = {
+				enable = true,
+			},
+			ensure_installed = {
+				"vimdoc",
+				"luadoc",
+				"vim",
+				"lua",
+				"markdown"
+			}
+		}
+	end,
     },
     -- Undo-Tree
     {
